d4c500c4a320de066b9286662ced0fe72fc8345c
Xheis <facebook.xheis@gmail.com>
2017-10-06 12:06:58 +1100

    MAKE PROGRAM: Fri 06/10/2017_12:06:58.68

--- commit.bat
+++ commit.bat
@@ -3,4 +3,4 @@ ECHO git add . | "C:\Program Files\Git\git-cmd.exe"
 set "DT=%date%_%time%"
 ECHO git commit -m "MAKE PROGRAM: %DT%" | "C:\Program Files\Git\git-cmd.exe"
 ECHO git push -u origin master | "C:\Program Files\Git\git-cmd.exe"
-
+ECHO "Programmed, committed and pushed to origin!"
