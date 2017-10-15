29f26c2ed6c7539bd2e0382c9530fc96a08cc966
Xheis <facebook.xheis@gmail.com>
2017-10-15 17:29:37 +1100

    MAKE NOT TESTED PROGRAM: Sun 15/10/2017_17:29:37.81

--- src/stepper.c
+++ src/stepper.c
@@ -30,9 +30,9 @@ void test_stepper()
     {
 		PORTD |= (1<<6);                //Make PORTD6 high to rotate motor in clockwise direction
 
-		for(x=0; x<4; x++)              //Give 50 pulses to rotate stepper motor by 90 degree's in full step mode
+		for(x=0; x<50; x++)              //Give 50 pulses to rotate stepper motor by 90 degree's in full step mode
 		{
-		 for(y=0; y<50; y++)
+		 for(y=0; y<10; y++)
 		 {
 		  PORTD |=(1<<7);
 		  _delay_us(700);
@@ -45,9 +45,9 @@ void test_stepper()
 		
 		PORTD &= ~(1<<6);              //Make PORTD6 high to rotate motor in anti-clockwise direction
 		
-		for(x=0; x<4; x++)             //Give 50 pulses to rotate stepper motor by 90 degree's in full step mode
+		for(x=0; x<50; x++)             //Give 50 pulses to rotate stepper motor by 90 degree's in full step mode
 		{
-			for(y=0; y<50; y++)
+			for(y=0; y<10; y++)
 			{
 				PORTD |=(1<<7);
 				_delay_us(700);
