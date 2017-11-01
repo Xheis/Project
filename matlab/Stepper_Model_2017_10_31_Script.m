fd84959b33ce98502e2b27122a480443ae469945
Xheis <facebook.xheis@gmail.com>
2017-11-01 22:13:23 +1100

    MAKE NOT TESTED PROGRAM: Wed 01/11/2017_22:13:23.62

--- matlab/Stepper_Model_2017_10_31_Script.m
+++ matlab/Stepper_Model_2017_10_31_Script.m
@@ -5,56 +5,40 @@ m = 0.2;          	% Mass of pendulum (kg)
 L = 0.8;            % Length of pendulum (m)
 wheel_Radius = 0.088;
 b = 0.01;           % Damping of cart (N.s/m) //misc, tuned
-c = 0.05;           % Damping of pendulum (N.m.s/rad) //calc'd from sys id
+c = 0.25;           % Damping of pendulum (N.m.s/rad) //calc'd from sys id
 
 % Other parameters
 g = 9.82;           % Acceleration due to gravity (m/s/s)
 %alpha = 1*pi/180;   % Ground slope (rad = deg*pi/180)
 theta0 = 5*pi/180;  % Initial angle (rad = deg*pi/180)
-
+dV0= 0;
 %% Calculating K-Gain
 % A =    [-c/(m*L*L),  0,      c/L;
 %         1/(m*L*L),   0,      1/L;
 %         0,          0,      0];
 %Original Matrixes-----------------
-A =    [-c/(m*L*L),  0,      c/L;
-        1/(m*L*L),   0,      1/L;
+A =    [-c/(m*L*L),  m*g*L, c/L;
+        1/(m*L*L),   0,     1/L;
         0,          0,      0];
 
+    
 B = [0;
      0;
      1];
  
- C = [0;0;0]
+ C = [0 0 0];
  
  D = 1;
 %----------------------------------
-N= 1/((C-D*K)/(B*K-A)*B+D);
 
-
-
-poles=[-5,-6,-7];
+poles=[-1,-8,-3];
 
 K=place(A,B,poles);
-
+N= 20;%1/((C-D*K)/(B*K-A)*B+D);
 %A_new = A-B*K;
 
-Controller_Gain = 1 %K(3);
-%%  
-%sym theta v x omega
-
-
-% -----------------------------------
-% Inner (angle) controller parameters
-%zeroes = 5.4277;%5.4277;
-%poles =  25.4277;%25.4277;
-%Controller_Gain = -119.0071;%-119.0071;
-% -----------------------------------
-
-% Outer (velocity) controller parameters
-TF_Theta_To_V_Gain = 1;
-TF_Theta_To_X_Gain = 1;
-
+%Controller_Gain = K(3);
+%%  Runnings the Simulation
 
 % Run simulation
 tsim = 30;
@@ -85,11 +69,11 @@ xlabel('Time (sec)')
 ylabel('x (m)')
 
 subplot(2,2,4)
-plot(t,V,t,r,'r')
+plot(t,V,t,ri,'r')
 grid on
 xlim([0 tsim])
 xlabel('Time (sec)')
-ylabel('v REF (m/s)')
+ylabel('ri (m/s)')
 legend('Actual','Demanded','Location','Best')
 
 figure(2);clf
