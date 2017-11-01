afed3fbb91ddb433dad74fcf83de3a95d95b525f
Xheis <facebook.xheis@gmail.com>
2017-11-01 22:29:44 +1100

    MAKE NOT TESTED PROGRAM: Wed 01/11/2017_22:29:44.00

--- matlab/Stepper_Model_2017_10_31_Script.m
+++ matlab/Stepper_Model_2017_10_31_Script.m
@@ -1,3 +1,7 @@
+clc
+clear all
+close all;
+
 %% Variables
 % Cart/pendulum parameters
 M = 1.2;              % Mass of cart (kg)
@@ -5,13 +9,14 @@ m = 0.2;          	% Mass of pendulum (kg)
 L = 0.8;            % Length of pendulum (m)
 wheel_Radius = 0.088;
 b = 0.01;           % Damping of cart (N.s/m) //misc, tuned
-c = 0.25;           % Damping of pendulum (N.m.s/rad) //calc'd from sys id
+c = 0.05;           % Damping of pendulum (N.m.s/rad) //calc'd from sys id
 
 % Other parameters
 g = 9.82;           % Acceleration due to gravity (m/s/s)
 %alpha = 1*pi/180;   % Ground slope (rad = deg*pi/180)
 theta0 = 5*pi/180;  % Initial angle (rad = deg*pi/180)
-dV0= 0;
+dV0 = 0.01;
+
 %% Calculating K-Gain
 % A =    [-c/(m*L*L),  0,      c/L;
 %         1/(m*L*L),   0,      1/L;
@@ -26,19 +31,33 @@ B = [0;
      0;
      1];
  
- C = [0 0 0];
+ C = [0 0 1];
  
- D = 1;
+ D = 0;
 %----------------------------------
 
-poles=[-1,-8,-3];
+poles=[-5,-6,-9];
 
 K=place(A,B,poles);
-N= 20;%1/((C-D*K)/(B*K-A)*B+D);
+N= 1/((C-D*K)/(B*K-A)*B+D);
 %A_new = A-B*K;
 
-%Controller_Gain = K(3);
-%%  Runnings the Simulation
+Controller_Gain = K(3);
+%%  
+%sym theta v x omega
+
+
+% -----------------------------------
+% Inner (angle) controller parameters
+%zeroes = 5.4277;%5.4277;
+%poles =  25.4277;%25.4277;
+%Controller_Gain = -119.0071;%-119.0071;
+% -----------------------------------
+
+% Outer (velocity) controller parameters
+% TF_Theta_To_V_Gain = 1;
+% TF_Theta_To_X_Gain = 1;
+
 
 % Run simulation
 tsim = 30;
@@ -69,11 +88,11 @@ xlabel('Time (sec)')
 ylabel('x (m)')
 
 subplot(2,2,4)
-plot(t,V,t,u,'r')
+plot(t,V,t,r,'r')
 grid on
 xlim([0 tsim])
 xlabel('Time (sec)')
-ylabel('Control Sig & Output (m/s)')
+ylabel('v REF (m/s)')
 legend('Actual','Demanded','Location','Best')
 
 figure(2);clf
