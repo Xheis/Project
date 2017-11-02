704e7c8ce4827b6ed0b5aa2df7bbef5511b00b5d
Xheis <facebook.xheis@gmail.com>
2017-11-02 20:47:06 +1100

    MAKE NOT TESTED PROGRAM: Thu 02/11/2017_20:47:06.59

--- src/encoder.c
+++ src/encoder.c
@@ -2,11 +2,104 @@
 #include <avr/io.h>
 #include "encoder.h"
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h> // atoi
+
+#include <ctype.h>
+#include <string.h>
+#include <inttypes.h> // For PRIxx and SCNxx macros
+#include <avr/pgmspace.h>
+
 static int32_t _count = 0;
 
 // Forward decl of private helper functions
 static void _encoder_enable_interrupts(void);
 static void _encoder_disable_interrupts(void);
+
+const A=2;
+const B=3;
+
+    //A = PD2 = int0
+    //B = PD3 = int1
+
+// const char ttable[6][4] = {
+//   {0x3 , 0x2, 0x1,  0x0}, {0x83, 0x0, 0x1,  0x0},
+//   {0x43, 0x2, 0x0,  0x0}, {0x3 , 0x5, 0x4,  0x0},
+//   {0x3 , 0x3, 0x4, 0x40}, {0x3 , 0x5, 0x3, 0x80}
+// };
+// Use the full-step state table (emits a code at 00 only)
+const char ttable[7][4] = {
+  {0x0, 0x2, 0x4,  0x0}, {0x3, 0x0, 0x1, 0x40},
+  {0x3, 0x2, 0x0,  0x0}, {0x3, 0x2, 0x1,  0x0},
+  {0x6, 0x0, 0x4,  0x0}, {0x6, 0x5, 0x0, 0x80},
+  {0x6, 0x5, 0x4,  0x0},
+};
+// // No complete step yet.
+// #define DIR_NONE 0x0
+// // Clockwise step.
+// #define DIR_CW 0x10
+// // Anti-clockwise step.
+// #define DIR_CCW 0x20
+
+// #define R_START 0x0
+
+// #define R_CCW_BEGIN 0x1
+// #define R_CW_BEGIN 0x2
+// #define R_START_M 0x3
+// #define R_CW_BEGIN_M 0x4
+// #define R_CCW_BEGIN_M 0x5
+// const unsigned char ttable[6][4] = {
+//   // R_START (00)
+//   {R_START_M,            R_CW_BEGIN,     R_CCW_BEGIN,  R_START},
+//   // R_CCW_BEGIN
+//   {R_START_M | DIR_CCW, R_START,        R_CCW_BEGIN,  R_START},
+//   // R_CW_BEGIN
+//   {R_START_M | DIR_CW,  R_CW_BEGIN,     R_START,      R_START},
+//   // R_START_M (11)
+//   {R_START_M,            R_CCW_BEGIN_M,  R_CW_BEGIN_M, R_START},
+//   // R_CW_BEGIN_M
+//   {R_START_M,            R_START_M,      R_CW_BEGIN_M, R_START | DIR_CW},
+//   // R_CCW_BEGIN_M
+//   {R_START_M,            R_CCW_BEGIN_M,  R_START_M,    R_START | DIR_CCW},
+// };
+
+/* Read input pins and process for events. Call this either from a
+ * loop or an interrupt (eg pin change or timer).
+ *
+ * Returns 0 on no event, otherwise 0x80 or 0x40 depending on the direction.
+ */
+char rotary_process(uint8_t A_in, uint8_t B_in);
+volatile char state = 0;
+char rotary_process(uint8_t A_in, uint8_t B_in)
+{
+    char pinstate = (A_in << 1) | B_in; //Create a hex 0xAB, to cross reference with our state LUT
+    state = ttable[state & 0xf][pinstate];
+    return (state & 0xc0);//0xc0);
+}
+
+void checkEnc(uint8_t A_in, uint8_t B_in);
+void checkEnc(uint8_t A_in, uint8_t B_in) 
+{
+    char result = rotary_process(A_in,B_in);
+    if (result)
+    {
+        if (result == 0x40)
+        {
+            printf_P(PSTR("Clockwise\n"));
+            _count--;   
+        }
+        else
+        {
+            printf_P(PSTR("Anti-clockwise\n"));
+            _count++;   
+        }
+    }
+    else
+    {
+    }
+}
+
 
 void encoder_init(void)
 {
@@ -17,7 +110,8 @@ void encoder_init(void)
     MCUCR = (1<<ISC11) | (0<<ISC10) |   //int1 ISC11:0 ISC10: 1 = Any logical change, from table 16-1 from datasheet
             (1<<ISC01) | (0<<ISC00);    //int0 ISC01:0 ISC00: 1 = Any logical change, from table 16-1 from datasheet
 
-
+    DDRD |= (0<<A) | (0<<B);
+    PORTD |= (1<<A) | (1<<B);
     // Set default count
     _count = 0;
 
@@ -25,14 +119,15 @@ void encoder_init(void)
     _encoder_enable_interrupts();
 }
 
+
 void encoder_edge_A_isr(void)
 {
     // TODO: Implement A edge logic to increment or decrement _count
     //A = PD2 = int0
     //B = PD3 = int1
 
-    int A = !(PINC & _BV(PC0));
-    int B = !(PINC & _BV(PC1));
+    int A = !(PIND & _BV(PD2));
+    int B = !(PIND & _BV(PD3));
     if (A==B) _count++;
     else _count--;
 }
@@ -43,11 +138,12 @@ void encoder_edge_B_isr(void)
     //A = PD2 = int0
     //B = PD3 = int1
 
-    int A = !(PINC & _BV(PC0));
-    int B = !(PINC & _BV(PC1));
+    int A = !(PIND & _BV(PD2));
+    int B = !(PIND & _BV(PD3));
     if (A==B) _count--;
     else _count++;
 }
+
 
 // void encoder_edge_A_isr(void)
 // {
@@ -55,8 +151,8 @@ void encoder_edge_B_isr(void)
 //     //A = PD2 = int0
 //     //B = PD3 = int1
 
-//     int A = !(PINA & _BV(PA0));
-//     int B = !(PINA & _BV(PA1));
+//     int A = !(PIND & _BV(PD2));
+//     int B = !(PIND & _BV(PD3));
 //     if (A==B) _count++;
 //     else _count--;
 // }
@@ -67,8 +163,8 @@ void encoder_edge_B_isr(void)
 //     //A = PD2 = int0
 //     //B = PD3 = int1
 
-//     int A = !(PINA & _BV(PA0));
-//     int B = !(PINA & _BV(PA1));
+//     int A = !(PIND & _BV(PD2));
+//     int B = !(PIND & _BV(PD3));
 //     if (A==B) _count--;
 //     else _count++;
 // }
@@ -104,7 +200,7 @@ void _encoder_enable_interrupts(void)
 {
     // TODO: Enable INT0 and INT1 interrupts
     //Enable INT0
-    GICR |= (1<<INT0) | (1<<INT1);
+    GICR |= (1<<INT0)| (1<<INT1);
     //Enable INT1
    // GICR |= (1<<INT1);
 }
@@ -118,3 +214,4 @@ void _encoder_disable_interrupts(void)
    // GICR |= (0<<INT1);
 }
 
+  
--- src/encoder_isr.c
+++ src/encoder_isr.c
@@ -1,12 +1,21 @@
 #include <avr/interrupt.h>
 #include "encoder.h"
 
+static uint8_t A = 0;
+static uint8_t B = 0;
 ISR(INT0_vect)
 {
-    encoder_edge_A_isr();
+    //encoder_edge_A_isr();
+
+	A = !(PIND & _BV(PD2));
+	B = !(PIND & _BV(PD3));
+    checkEnc(A,B);
 }
 
 ISR(INT1_vect)
 {
-    encoder_edge_B_isr();
+    //encoder_edge_B_isr();
+	A = !(PIND & _BV(PD2));
+	B = !(PIND & _BV(PD3));
+    checkEnc(A,B);
 }
